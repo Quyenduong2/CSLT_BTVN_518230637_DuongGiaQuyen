d6475472f13dfc89ebfbb02df9642aa79984069b
Quyen <dgiaquyen40@gmail.com>
2023-11-30 00:19:32 +0700

    bai tap co so lap trinh

---
+++ src/Calculator.java
@@ -0,0 +1,57 @@
+//public class Calculator {
+//    public static void main(String[] args) {
+//        String name = "Horus";
+//        int age = 24;   // khai bao kieu so nguyen int
+//
+//        System.out.println(name);
+//        System.out.println(age);
+//
+//        int number1 = 20;
+//        int number2 = 40;
+//
+//        System.out.println(number1 + number2);
+//
+//        byte byteNumber = 1;
+//        short shortNumber = 100;
+//        int intNumber = 1000000;
+//        long longNumber = 1000000000000L; // L
+//
+//        float floatPi = 3.14159265f;
+//        double doublePi = 3.14159265;
+//
+//        System.out.println(floatPi);
+//        System.out.println(doublePi);
+//
+//        char character = 'a'; // chi luu dc 1 ki tu
+//
+//        boolean trueValue = true;
+//        boolean falseValue = false;
+//
+//        System.out.println(character);
+//        System.out.println(trueValue);
+//        System.out.println(falseValue);
+//
+//        String name2 = "Gia Tri";
+//        String oneCharacter = "a";
+//
+//        System.out.println(name2);
+//        System.out.println(name.toUpperCase()) ;
+//
+//
+//
+//        int[] gameScores = {78, 92, 87, 63, 105, 98, 80, 75};
+//        int totalScore, bestScoreGameIndex, worstScoreGameIndex;
+//        double averageScore;
+//
+//        totalScore = 0;
+//        for( int i = 0; i < gameScores.length; i++) {
+//            totalScore =  totalScore + gameScores[i];
+//            averageScore = totalScore / gameScores.length -1;
+//        }
+//
+//        System.out.println("Average Score: " + averageScore);
+//
+//    }
+//}
+//
+//
---
+++ src/baiTap.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class baiTap {
+    public static void main(String[] args) {
+        //BT1
+//        System.out.println("HelloHello \nHello\t\tHello\n\nHello\t\t\t\t\tHello");
+
+        //BT2 The Works of Wolfgang
+
+//        System.out.print("The works of Wolfgang\ninclude ");
+//        System.out.print("the following");
+//        System.out.print("\nThe Turkish March ");
+//        System.out.print("and Symphony No. 40 ");
+//        System.out.println("in G minor.");
+
+//        BT3
+//        int little;
+//        int big;
+//        little = 2;
+//        big = 2000;
+//        System.out.println("The little number is " + little);
+//        System.out.println("The big number is " + big);
+
+//        1) Viết chương trình nhập xuất ra màn hình thông tin của mỗi sinh viên theo mẫu sau:
+//        Ho ten:
+//        Ma so sinh vien:
+//        Lop:
+//        So dien thoai: Gioi tinh:
+
+//        System.out.println("Ho Ten: ");
+//        System.out.println("Ma so sinh vien:");
+//        System.out.println("Lop: ");
+//        System.out.println("So dien thoai:\t\t\t\t\tGioi tinh:");
+
+//        2) Viết chương trình xuất ra màn hình bài thơ sau:
+//        System.out.println("XUÂN VỀ");
+//        System.out.println("Từng đàn con trẻ chạy xun xoe");
+//        System.out.println("Mưa lạnh, trời quang, nắng mới hoe");
+//        System.out.println("Lá nõn nhành non, ai tráng bạc?");
+//        System.out.println("Gió về từng tận, gió bay đi...\n");
+//        System.out.println("\tThong thả dân gian nghỉ việc đồng");
+//        System.out.println("\tLúa thì con gái mượt như nhung");
+//        System.out.println("\tĐầy vươn hoa bưởi, hoa cam rụng");
+//        System.out.println("\tNgào ngạt hương bay, bướm vẽ vòng\n");
+//        System.out.println(" Trên đường cát mịn, một đôi cô");
+//        System.out.println(" Yếm đỏ, khăn thâm, trẩy hội chùa");
+//        System.out.println(" Gậy trúc dắt bà già tóc bạc");
+//        System.out.println(" Tay lần tràng hạt, miệng nam mô.");
+
+//        3) Viết chương trình xuất ra màn hình theo mẫu sau:
+//        System.out.println("/*****************************************/");
+//        System.out.println("/******* HUONG DAN CHEP TAP TIN *******/");
+//        System.out.println("/=>B1. Vao thu muc “C:\\TUYENTAP\\thotinh.txt” /");
+//        System.out.println("/=>B2. Click chuot phai vao tap tin thotinh.txt /");
+//        System.out.println("/=>B3. Chon copy tu menu tat ");
+//        System.out.println("/=>B4. Chon vi tri can luu, click phai chon paste /");
+//        System.out.println("/*****************************************/");
+
+//        4) Viết chương trình nhập vào 2 số nguyên dương a và b, cho biết kết quả chia lấy phần
+//        nguyên và phần dư của a với b.
+
+//        double a ,b;
+//
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        System.out.print("Nhap a: ");
+//        a = keyboard.nextDouble();
+//        System.out.print("Nhap b: ");
+//        b = keyboard.nextDouble();
+//
+//        System.out.println("KQ Chia lay phan nguyen: " + (a/b));
+//        System.out.println("KQ Chia lay phan du: " + (a%b));
+
+//        5) Viết chương trình nhập một số nguyên dương N có 2 chữ số từ bàn phím, xuất ra màn
+//        hình tổng các chữ số của N
+
+//        int a,b,S = 0;
+//
+//        Scanner keyboard = new Scanner(System.in);
+//
+//        System.out.print("Nhap so nguyen duong co 2 chu so: ");
+//        a = keyboard.nextInt();
+//
+//        S = (a/10) + ( a%10);
+//
+//        System.out.println("S = " + S);
+    }
+}
